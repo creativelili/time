a1474c3caba1113a4fcd0562de1cc14430026644
creativelili <132809882+creativelili@users.noreply.github.com>
2026-09-17 08:53:31 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="time-cosmic-v7-pwa";
+const C="time-cosmic-v8-install";
 const A=["./","./index.html","./manifest.json","./css/style.css","./js/clock.js","./js/alarm.js","./js/app.js","./cosmic-bg.png","./icons/icon-192.png","./icons/icon-512.png","./icons/apple-touch-icon.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
